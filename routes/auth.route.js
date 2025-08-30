734a66991f2b5ce93c60d6e20e26193d7465aa96
Lawrence9908 <mugwenalawrence121@gmail.com>
2025-08-31 01:38:51 +0200

    update user auth profile endpoint from post to get

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,13 +2,13 @@ import express from "express";
 import { login, logout, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
 import { protectedRoute } from "../middleware/auth.middleware.js";
 
-const router  = express.Router();
+const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/refresh-token', refreshToken);
-router.post('/profile', getProfile);
+router.get('/profile', getProfile);
 
 
 export default router;
